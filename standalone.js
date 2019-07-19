43703108bdfccfdfbf9c3dcae9cb173c606cc43f
jonkofee <jonkofee@icloud.com>
2019-07-19 22:19:38 +0300

    ⚡️ hardcode conf

--- standalone.js
+++ standalone.js
@@ -3,14 +3,11 @@ const minimist = require('minimist')
 const hap = require('hap-nodejs')
 const CameraAccessory = require('./CameraAccessory')(hap, hap.Accessory, console.log)
 
-let conf = {}
-const argv = minimist(process.argv.slice(2))
-const configFile = argv['c'] || argv['config']
-if (configFile) {
-  try {
-    conf = require(configFile)
-  } catch (e) { if (e.code !== 'MODULE_NOT_FOUND') { throw e } }
-}
+let conf = {
+    username: 'EC:23:3D:D3:CE:00',
+    pincode: '031-45-150',
+    debug: true
+};
 
 console.log('HAP-NodeJS starting...')
 
@@ -18,17 +15,15 @@ hap.init()
 
 const cameraAccessory = new CameraAccessory(conf)
 
-const pincode = conf.pincode || '031-45-154'
-
 cameraAccessory.publish({
-  username: conf.username || 'EC:23:3D:D3:CE:CE',
-  pincode: pincode,
+  username: conf.username,
+  pincode: conf.pincode,
   category: hap.Accessory.Categories.CAMERA
 }, true)
 
 console.log('Scan this code with your HomeKit App on your iOS device to pair with Camera:')
 console.log('                       ')
 console.log('    ┌────────────┐     ')
-console.log(`    │ ${pincode} │     `)
+console.log(`    │ ${conf.pincode} │     `)
 console.log('    └────────────┘     ')
 console.log('                       ')
