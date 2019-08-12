681b8ffe04de7bb5b660be3d39ff04ed1cbeb608
jonkofee <jonkofee@icloud.com>
2019-08-12 17:40:03 +0300

    🔥 rm lock

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ const hap = require('hap-nodejs');
 const Bridge = hap.Bridge;
 const Accessory = hap.Accessory;
 const CameraAccessory = require('./CameraAccessory');
-const LockAccessory = require('./LockAccessory');
 
 hap.init();
 
 const bridge = new Bridge('Camera', hap.uuid.generate("homebridge-camera-rpi:bridge"));
 
 bridge.addBridgedAccessory(new CameraAccessory());
-bridge.addBridgedAccessory(new LockAccessory());
 
 bridge.publish({
-  username: 'EC:13:12:13:8E:07',
+  username: 'EC:13:12:13:1E:07',
   pincode: '031-45-150',
   category: Accessory.Categories.BRIDGE
 }, true);
