9033aacab297f06319154a5d5b138603a65dc3d0
jonkofee <jonkofee@icloud.com>
2019-08-12 20:12:10 +0300

    ⚡️ button

--- CameraAccessory.js
+++ CameraAccessory.js
@@ -1,7 +1,11 @@
 'use strict';
 const hap = require('hap-nodejs');
+const Gpio = require('onoff').Gpio;
 const Accessory = hap.Accessory;
 const Service = hap.Service;
+const Characteristic = hap.Characteristic;
+
+const buttonGpio = new Gpio(17, 'in', 'falling', {debounceTimeout: 10});
 
 const CameraSource = require('./CameraSource');
 
@@ -11,9 +15,19 @@ module.exports = class CameraAccessory extends Accessory {
 
     this.addService(new Service.Speaker("Speaker"));
 
+    var button = new Service.Doorbell('Button');
+
+    this.addService(button);
+
     this.on('identify', (paired, callback) => {
         console.log('Idetify camera');
         callback();
+    });
+
+
+    buttonGpio.watch(() => {
+        button.getCharacteristic(Characteristic.ProgrammableSwitchEvent).setValue(1);
+        console.log('alarm')
     });
 
     this.configureCameraSource(new CameraSource());
